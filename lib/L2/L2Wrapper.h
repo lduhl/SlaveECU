ed9dd85a88b6fd2801c52255e46829ea33ef5cd5
Andrei <duh@vnet.ee>
2022-12-26 23:43:45 +0200

    first commit

--- lib/L2/L2Wrapper.h
+++ lib/L2/L2Wrapper.h
@@ -4,11 +4,26 @@
 
 #pragma once
 
+#include "can.h"
+#include "usart.h"
+#include "gpio.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_can.h"
+
+
 class L2Wrapper
 {
     public:
-        L2Wrapper();
+        L2Wrapper()
+        {
+
+        }
         
     private:
-        
+    CAN_TxHeaderTypeDef TxHeader;
+	CAN_RxHeaderTypeDef RxHeader;
+	uint8_t TxData[8] = {0,};
+	uint8_t RxData[8] = {0,};
+	uint32_t TxMailbox = 0;
+	uint8_t trans_str[30];
 };
